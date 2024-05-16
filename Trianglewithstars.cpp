4b4fa6047dd330613a90a8c4c7d91ad7830f4de0
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:17:11 +0300

    added Trianglewithstars.cpp

---
+++ Trianglewithstars.cpp
@@ -0,0 +1,22 @@
+//  PROGRAM THAT MAKING TRIANGLE WITH STARS
+
+/*#include <iostream>
+
+int main(){
+    int n;
+    std::cout << "enter length of the triangle : " << std::endl;
+    std::cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            std::cout << " " ;
+        }
+        
+        for(int k=1;k<=2*i-1;k++){
+            std::cout <<"*";
+        }
+        std::cout << "\n";
+    }
+    
+    
+    return 0 ;
+}*/
