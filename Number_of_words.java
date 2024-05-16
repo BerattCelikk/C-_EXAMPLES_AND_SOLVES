b0908c76b4bb64bd4d1d21ef49bb215e5e099f2f
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:14:12 +0300

    added Number_of_words.cpp

---
+++ Number_of_words.java
@@ -0,0 +1,17 @@
+//  PROGRAM THAT FINDS THE NUMBER OF TOTAL WORDS IN A SENTENCE
+/*#include <iostream>
+#include <sstream>
+
+int main(){
+    std::string text;
+    std::cout << "enter a text \n";
+    std::getline(std::cin,text);
+    std::istringstream iss(text);
+    int count=0;
+    std::string word;
+    while(iss>>word){
+        count++;
+    }
+    std::cout << "this text includes total "<<count<<"words."<< std::endl;
+    return 0 ;
+}*/
