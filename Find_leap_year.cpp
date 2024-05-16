4fbde6751b647b1fa739053a8ed738f771855b17
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:11:52 +0300

    added Find_leap_year.cpp

---
+++ Find_leap_year.cpp
@@ -0,0 +1,23 @@
+//  PROGRAM THAT FINDS LEAP YEARS.((LEAP YEAR % 4==0)  && (LEAP YEAR % 400 == 0) && (LEAP YEAR % 100 != 0))
+
+/*#include <iostream>
+int main() {
+    int year;
+    std::cout << "Enter a year: " << std::endl;
+    std::cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 != 0) {
+            if (year % 400 == 0) {
+                std::cout << "This year is a leap year" << std::endl;
+            } else {
+                std::cout << "This year is not a leap year" << std::endl;
+            }
+        } else {
+            std::cout << "This year is not a leap year" << std::endl;
+        }
+    } else {
+        std::cout << "This year is not a leap year" << std::endl;
+    }
+    return 0;
+}*/
