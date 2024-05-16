d4f64a575cd107d5deb79d3a23c2b84c1c1f1230
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:42:55 +0300

    added reverse_right_triangle.cpp

---
+++ reverse_right_triangle.cpp
@@ -0,0 +1,19 @@
+/*Creating an reverse right triangle with stars*/
+
+/*#include <iostream>
+
+int main(){
+    int n;
+    std::cout << "enter length of the reverse right triangle" << std::endl;
+    std::cin >> n;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            std::cout << " ";
+        }
+        for(int k=1;k<=i;k++){
+            std::cout << "*";
+        }
+        std::cout <<"\n";
+    }
+    return 0 ;
+}*/
