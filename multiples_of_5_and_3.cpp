106bf051ddfea50551a6604e71c29a7c581d0b5b
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:41:44 +0300

    added multiples_of_5_and_3.cpp

---
+++ multiples_of_5_and_3.cpp
@@ -0,0 +1,22 @@
+/*Printing the multiples of 5 and 3 between 2 numbers entered externally*/
+/*#include <iostream>
+#include <cmath>
+int main(){
+    int a,b;
+    std::cout << "enter a number" << std::endl;
+    std::cin >> a;
+    std::cout << "enter a number" << std::endl;
+    std::cin >> b;
+    int min=std::min(a,b);
+    int max=std::max(a,b);
+    std::cout << "Divisible by 15 : " << std::endl;
+    for(int i=min;i<max;i++){
+        if(i%15==0){
+            std::cout << i << std::endl;
+        }
+    }
+    
+    
+    
+    return 0 ;
+}*/
