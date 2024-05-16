7fb3926416dc945ce5acff250251d406cb15014b
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:39:06 +0300

    added geometric_average.cpp

---
+++ geometric_average.cpp
@@ -0,0 +1,18 @@
+//  FIND GEOMETRIC AVERAGE
+/*#include <iostream>
+#include <cmath>
+int main(){
+    double n;
+    std::cout << "enter a number" << std::endl;
+    std::cin >> n;
+    int a=1;
+    for(int i=1;i<=n;i++){
+        a*=i;
+    }
+    double geometric=std::pow((double)a,1.0/n);
+    std::cout << "geometric average ="<<geometric << std::endl;
+    
+    
+    
+    return 0 ;
+}*/
