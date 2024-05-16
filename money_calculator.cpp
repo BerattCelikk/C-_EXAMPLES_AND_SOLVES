e7688024ffbd593ce25b37c37ebe4f7e6d61c7ed
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:41:06 +0300

    added money_calculator.cpp

---
+++ money_calculator.cpp
@@ -0,0 +1,21 @@
+/*A clerk wants to withdraw money from an ATM machine. Only 10, 20, 50 and
+There are 100 TL banknotes left. Maximum number of large denomination banknotes,
+C, which tells you on the screen how many 100, 50, 20 and 10 lira banknotes the money to be withdrawn will consist of.
+Write the program.*/
+
+/*#include <iostream>
+
+int main(){
+    int money;
+    std::cout << "enter your money amount" << std::endl;
+    std::cin >> money;
+    int hundred = money/100;
+    money%=100;
+    int fifty=money/50;
+    money%=50;
+    int twenty=money/20;
+    money%=20;
+    int ten=money/10;
+    std::cout << "100 ="<<hundred<<" 50 = "<<fifty<<" 20 = "<<twenty<<" 10 = "<<ten;
+    return 0 ;
+}*/
