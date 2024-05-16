1ad1fc856ecf664e202a733b48d7666a08d9e2c4
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:12:16 +0300

    added Harmonicnumber.cpp

---
+++ Harmonicnumber.cpp
@@ -0,0 +1,18 @@
+//PROGRAM THAT CALCULATE HARMONIK AVERAGE UP TO N.(USER WİLL ENTER THE N).
+
+/*#include <iostream>
+
+int main(){
+    int n;
+    double a=0,harmonic=0;
+    std::cout << "enter a number :" << std::endl;
+    std::cin >> n;
+    for(int i=1;i<=n;i++){
+        a+=(1.0/i);
+    }
+    harmonic=n/a;
+    std::cout << "harmonic average ="<<harmonic << std::endl;
+    
+    
+    return 0 ;
+}*/
