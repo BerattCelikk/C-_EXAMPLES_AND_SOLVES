1c9c362ec45a820cf41b100a74f66744b8a6ea44
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:39:59 +0300

    added isvowel.cpp

---
+++ isvowel.cpp
@@ -0,0 +1,27 @@
+/*Program to Find Out Whether a Letter Entered from the Keyboard is a Vowel*/
+
+/*#include <iostream>
+#include <cctype>
+int main(){
+    char c;
+    std::cout << "enter a letter" << std::endl;
+    std::cin >> c;
+    bool isletter=false;
+    if(std::isalpha(c)){
+        c=std::tolower(c);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            isletter=true;
+        }
+        if(isletter){
+            std::cout << "this letter is a vowel" << std::endl;
+        }else{
+            std::cout << "this letter is not a vowel" << std::endl;
+        }
+        
+    }else{
+        std::cout << "this character is not a letter." << std::endl;
+    }
+    
+    
+    return 0 ;
+}*/
