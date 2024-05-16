a4cca30a7395a8cec8a3620417033f27eeb341ca
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:13:45 +0300

    added Maxandmin.cpp

---
+++ Maxandmin.cpp
@@ -0,0 +1,27 @@
+//PROGRAM THAT FINDS MAX AND MIN IN ARRAY
+
+/*#include <iostream>
+#include <vector>
+
+int main(){
+    int n;
+    std::cout << "enter length of the array :" << std::endl;
+    std::cin >> n;
+    std::vector<int> array(n) ;
+    for(int i=0;i<n;i++){
+        std::cout << "enter a number :" << std::endl;
+        std::cin >> array[i];
+    }
+    int max=array[0],min=array[0];
+    for(int i=0;i<n;i++){
+        if(max<array[i]){
+            max=array[i];
+        }else if(min>array[i]){
+            min=array[i];
+        }
+    }
+    std::cout << "max ="<<max<<"and min ="<<min<< std::endl;
+    
+    
+    return 0 ;
+}*/
