f14f6aa4bf25c930d5c4be16003d5ccc115f073a
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:13:17 +0300

    added Largest_to_Smallest.cpp

---
+++ Largest_to_Smallest.java
@@ -0,0 +1,28 @@
+//  a-)SORT FROM LARGEST TO SMALLEST
+
+/*#include <iostream>
+#include <vector>
+int main(){
+    int n;
+    std::cout <<"enter length of the array :"<<"\n";
+    std::cin >> n;
+    std::vector<int> array(n) ;
+    for(int i=0;i<n;i++){
+        std::cout <<"enter a number\n";
+        std::cin >> array[i] ;
+    }
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(array[j]<array[j+1]){
+                temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        std::cout <<array[i] <<"  ";
+    }
+    return 0 ;
+}*/
