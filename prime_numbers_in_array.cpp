165282558dcbc761fbda62a6dcfae91a84692389
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 21:42:33 +0300

    added prime_numbers_in_array.cpp

---
+++ prime_numbers_in_array.cpp
@@ -0,0 +1,47 @@
+/*Code the program that finds and displays the prime numbers of an array*/
+
+/*#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cout << "Enter length of the array: ";
+    std::cin >> n;
+
+    std::vector<int> array(n);
+
+
+    for (int i = 0; i < n; i++) {
+        std::cout << "Enter a number: ";
+        std::cin >> array[i];
+    }
+
+    std::cout << "Prime Numbers: ";
+
+
+    for (int i = 0; i < n; i++) {
+        int number1 = array[i];
+        bool isprime = true;
+
+
+        if (sayi <= 1) {
+            isprime = false;
+        } else {
+
+            for (int j = 2; j * j <= sayi; j++) {
+                if (number1 % j == 0) {
+                    isprime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isprime) {
+            std::cout << number1 << " ";
+        }
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}*/
