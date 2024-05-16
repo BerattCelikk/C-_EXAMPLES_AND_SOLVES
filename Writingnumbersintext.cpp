8268b4f5d36e2801262e9213f7239ab6eabd7aaa
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-16 11:17:27 +0300

    added Writingnumbersintext.cpp

---
+++ Writingnumbersintext.cpp
@@ -0,0 +1,19 @@
+//PROGRAM THAT WRITING NUMBERS IN TEXT
+
+/*#include <iostream>
+#include <sstream>
+int main(){
+    int number;
+    std::cout << "enter a number :" << std::endl;
+    std::cin >> number;
+    int thousand=number/1000;
+    int hundred=(number/100)%10;
+    int tens=(number/10)%10;
+    int units=(number%10);
+    std::string thousnd[]{" ","one thousand","two thousand","three thousand","four thousand","five thousand","six thousand","seven thousand","eight thousand","nine thousand"};
+    std::string hundrd[]={" ","one hundred","two hundred","three hundred","four hundred","five hundred"," six hundred","seven hundred","eight hundred","nine hundred"};
+    std::string tns[]={" ","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    std::string unit[]={" ","one","two","three","four","five","six","seven","eight","nine"};
+    std::cout << thousnd[thousand]<<"  "<<hundrd[hundred]<<"  "<<tns[tens]<<"  "<<unit[units] << std::endl;
+    return 0 ;
+}*/
